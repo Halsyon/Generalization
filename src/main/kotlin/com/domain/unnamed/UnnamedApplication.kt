ad6bc8af3f5aab8d266a8251e92a64f1e83e2557
GrandEmetak <JekHamond@tutanota.com>
2023-04-22 19:37:53 +0500

    feat: Initial commit

---
+++ src/main/kotlin/com/domain/unnamed/UnnamedApplication.kt
@@ -0,0 +1,11 @@
+package com.domain.unnamed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UnnamedApplication
+
+fun main(args: Array<String>) {
+    runApplication<UnnamedApplication>(*args)
+}
---
+++ src/test/kotlin/com/domain/unnamed/UnnamedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.domain.unnamed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnnamedApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
